5c80fb85b1ad7c09523bc01ec65f76e4a8e2026b
Graham Campbell <graham@alt-three.com>
2017-04-18 12:07:50 +0100

    Retry on 429

--- src/GuzzleFactory.php
+++ src/GuzzleFactory.php
@@ -41,7 +41,7 @@ final class GuzzleFactory
         $stack = HandlerStack::create();
 
         $stack->push(Middleware::retry(function ($retries, RequestInterface $request, ResponseInterface $response = null, TransferException $exception = null) {
-            return $retries < 3 && ($exception instanceof ConnectException || ($response && $response->getStatusCode() >= 500));
+            return $retries < 3 && ($exception instanceof ConnectException || ($response && ($response->getStatusCode() >= 500 || $response->getStatusCode() === 429)));
         }, function ($retries) {
             return (int) pow(2, $retries) * $backoff;
         }));
