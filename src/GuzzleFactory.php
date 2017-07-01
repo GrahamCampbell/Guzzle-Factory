2ab85ccd9c636ca5665bb504a541586de710c3cd
Graham Campbell <graham@alt-three.com>
2017-07-01 16:35:39 +0100

    Allow the 4xx retry codes to be customized

--- src/GuzzleFactory.php
+++ src/GuzzleFactory.php
@@ -29,33 +29,54 @@ use Psr\Http\Message\ResponseInterface;
 final class GuzzleFactory
 {
     /**
+     * The default connect timeout.
+     *
+     * @var int
+     */
+    const CONNECT_TIMEOUT = 10;
+
+    /**
+     * The default connect timeout.
+     *
+     * @var int
+     */
+    const TIMEOUT = 15;
+
+    /**
+     * The default 4xx retry codes.
+     *
+     * @var int[]
+     */
+    const CODES = [429];
+
+    /**
      * Create a new guzzle client.
      *
-     * @param array $options
-     * @param int   $backoff
+     * @param array      $options
+     * @param int        $backoff
+     * @param int[]|null $codes
      *
      * @return \GuzzleHttp\Client
      */
-    public static function make(array $options = [], int $backoff = 1000)
+    public static function make(array $options = [], int $backoff = 1000, array $codes = null)
     {
-        $stack = static::handler($backoff);
-
-        return new Client(array_merge(['handler' => $stack, 'connect_timeout' => 10, 'timeout' => 15], $options));
+        return new Client(array_merge(['handler' => self::handler($backoff, $codes), 'connect_timeout' => self::CONNECT_TIMEOUT, 'timeout' => self::TIMEOUT], $options));
     }
 
     /**
      * Create a new guzzle handler stack.
      *
-     * @param int $backoff
+     * @param int        $backoff
+     * @param int[]|null $codes
      *
      * @return \GuzzleHttp\Client
      */
-    public static function handler(int $backoff = 1000)
+    public static function handler(int $backoff = 1000, array $codes = null)
     {
         $stack = HandlerStack::create();
 
         $stack->push(Middleware::retry(function ($retries, RequestInterface $request, ResponseInterface $response = null, TransferException $exception = null) {
-            return $retries < 3 && ($exception instanceof ConnectException || ($response && ($response->getStatusCode() >= 500 || $response->getStatusCode() === 429)));
+            return $retries < 3 && ($exception instanceof ConnectException || ($response && ($response->getStatusCode() >= 500 || in_array($codes === null ? self::CODES : $codes, $response->getStatusCode()))));
         }, function ($retries) use ($backoff) {
             return (int) pow(2, $retries) * $backoff;
         }));
